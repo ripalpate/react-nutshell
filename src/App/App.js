49f88e563682daba2607403f473ae6fdf9c0c2f1
Ripal Patel <ripal212007@gmail.com>
2019-01-07 19:21:04 -0600

    login is working

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import connection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
 import './App.scss';
 
 
 class App extends Component {
+  componentDidMount() {
+    connection();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <button className='btn btn-danger'>HELP ME</button>
-          <Button
-    tag="a"
-    color="success"
-    size="large"
-    href="http://reactstrap.github.io"
-    target="_blank"
->
-    View Reactstrap Docs
-</Button>
-        </header>
+        <Auth/>
       </div>
     );
   }
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import authRequests from '../../helpers/data/authRequests';
+import './Auth.scss';
+
+class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+
+    }).catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div className="Auth">
+        <button className="btn btn-danger" onClick={this.authenticateUser}>Login</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
