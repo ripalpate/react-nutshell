305c1985dbd32a348daae3d4f4041f9c22eca091
Ripal Patel <ripal212007@gmail.com>
2019-01-13 14:16:34 -0600

    added delete button

--- src/components/pages/SingleMessages/SingleMessage.js
+++ src/components/pages/SingleMessages/SingleMessage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SingleMessage.scss';
 import messagesShape from '../../../helpers/propz/messagesShape';
+import authRequests from '../../../helpers/data/authRequests';
 
 class SingleMessage extends React.Component {
   static propTypes = {
@@ -9,10 +10,25 @@ class SingleMessage extends React.Component {
 
   render() {
     const { message } = this.props;
+    const uid = authRequests.getCurrentUid();
+    const makeButtons = () => {
+      if (message.uid === uid) {
+        return (
+            <span className="col text-right">
+              <button className="btn btn-danger">
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </span>
+        );
+      }
+      return <span className="col"></span>;
+    };
+
     return (
       <div className="singleMessage row mt-3">
         <span className="col user">{message.userName}</span>
         <span className="col message">{message.message}</span>
+        {makeButtons()}
       </div>
     );
   }
